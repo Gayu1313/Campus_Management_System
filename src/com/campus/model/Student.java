d142b99c22837a5bbbb820a34ef9c9a981e8baaa
Gayu1313 <gayatherigayatheri29@gmail.com>
2026-09-23 15:39:59 +0530

    Day3

---
+++ src/com/campus/model/Student.java
@@ -0,0 +1,69 @@
+package com.campus.model;
+ 
+public class Student {
+//Encapsulation data hiding
+//Instance variables
+   private int studentid;
+   private String studentname;
+   private int age;
+   private String department;
+   private int[] marks;
+
+   //static variables
+   static int studentCount = 0;
+   
+   //Default Constructor
+   public Student() {
+    studentCount++;
+   }
+
+   //parameterized constructor
+   public Student(int studentid, String studentname, int age, String department, int[] marks){
+    this.studentid = studentid;
+    this.studentname = studentname;
+    this.age = age;
+    this.department = department;
+    this.marks = marks;
+    studentCount++;
+   }
+   //getters
+   public int getStudentid() {
+    return studentid;
+   }
+   public String getStudentname() {
+    return studentname;
+   }
+   public int getAge() {
+    return age;
+   }
+   public String getDepartment() {
+    return department;
+   }
+   public int[] getMarks() {
+    return marks;
+   }
+
+   //Setters
+   
+   public void setStudentid(int studentid){
+    this.studentid = studentid;
+   }
+   public void setStudentname(String studentname){
+    this.studentname = studentname;
+   }
+   public void setAge(int age){
+    this.age = age;
+   }
+   public void setDepartment(String department){
+    this.department = department;
+   }
+   public void setMarks(int[] marks){
+    this.marks = marks;
+   }
+
+   public void displayStudentInfo(boolean showMarks){
+    if(showMarks) {
+        System.out.println("Marks:" +java.util.Arrays.toString(marks));
+    }
+   }
+}
